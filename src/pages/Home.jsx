b13876c41c44cca9da7c6b155f421120023ef3f0
iftharkamal <iftharkamal@gmail.com>
2024-11-11 06:36:39 +0530

    our policy error change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import Bestseller from '../components/Bestseller'
-import OurPolicy from '../components/Ourpolicy'
+import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
 
 const Home = () => {
